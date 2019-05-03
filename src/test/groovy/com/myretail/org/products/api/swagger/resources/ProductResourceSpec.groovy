f47acdda69fd9290b5a09ab6c45737adc4df08c8
ocon0178 <ocon0178@gmail.com>
2019-05-03 11:52:24 -0500

    Added spock framework and spec for product mapper

---
+++ src/test/groovy/com/myretail/org/products/api/swagger/resources/ProductResourceSpec.groovy
@@ -0,0 +1,40 @@
+package com.myretail.org.products.api.swagger.resources
+
+import com.myretail.org.products.api.swagger.model.Product
+import com.myretail.org.products.api.swagger.model.ProductCurrentPrice
+import com.myretail.org.products.domain.CurrencyCode
+import com.myretail.org.products.domain.Price
+import com.myretail.org.products.domain.ProductEntity
+import spock.lang.Specification
+
+class ProductResourceSpec extends Specification {
+
+  def "maps product resource to domain"() {
+    given:
+    Product resource = new Product(id: 123456, name: "test", currentPrice: new ProductCurrentPrice(value: 55.12, currencyCode: "CAD"))
+    when:
+    def domain = ProductResource.toDomain(resource)
+    then:
+    with(domain) {
+      id == 123456
+      name == "test"
+      price.value == 55.12
+      price.currencyCode == CurrencyCode.CAD
+    }
+
+  }
+
+  def "maps product domain to resource"() {
+    given:
+    ProductEntity domain = new ProductEntity(987654321, "domain", new Price(1000.22, CurrencyCode.EUD))
+    when:
+    def resource = ProductResource.toResource(domain)
+    then:
+    with(resource) {
+      id == 987654321
+      name == "domain"
+      currentPrice.value == 1000.22
+      currentPrice.currencyCode == CurrencyCode.EUD.name()
+    }
+  }
+}
