139a4cd7cc494b2d65ba4bfd0906b9386299afe3
ocon0178 <ocon0178@gmail.com>
2019-05-03 21:10:33 -0500

    Added back end mongodb services and test cases.  Implemented product put and test cases.

--- src/test/java/com/myretail/org/products/api/swagger/api/ProductsApiEndpointIntegrationTest.java
+++ src/test/java/com/myretail/org/products/api/swagger/api/ProductsApiEndpointIntegrationTest.java
@@ -2,6 +2,7 @@ package com.myretail.org.products.api.swagger.api;
 
 import com.myretail.org.products.api.IProductResourceService;
 import com.myretail.org.products.domain.CurrencyCode;
+import com.myretail.org.products.domain.EntityNotFoundException;
 import com.myretail.org.products.domain.Price;
 import com.myretail.org.products.domain.ProductEntity;
 import org.junit.Test;
@@ -10,14 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -48,6 +53,35 @@ public class ProductsApiEndpointIntegrationTest {
     //then
     this.mvc.perform(get("/products/{id}", id)).andExpect(status().isOk()).andExpect(content().json(expected,true));
   }
+
+  @Test
+  public void productByIdNotFound() throws Exception {
+    //given
+    int id = 15117729;
+    String expectedMessage = "Unable to find the product.";
+    when(productResourceService.getProduct(id)).thenThrow(new EntityNotFoundException(expectedMessage));
+    //then
+    this.mvc.perform(get("/products/{id}", id)).andExpect(status().isNotFound()).andExpect(status().reason(expectedMessage));
+  }
+
+  @Test
+  public void updateProductById() throws Exception {
+    //given
+    String body = "{\"id\":15117729,\"name\":\"blah\",\"current_price\":{\"value\":99.66,\"currency_code\":\"USD\"}}";
+    //then
+    this.mvc.perform(put("/products/{id}", 12345).contentType(MediaType.APPLICATION_JSON_UTF8).content(body)).andExpect(status().isOk());
+  }
+
+  @Test
+  public void updateProductByIdNotFound() throws Exception {
+    //given
+    String body = "{\"id\":15117729,\"name\":\"blah\",\"current_price\":{\"value\":99.66,\"currency_code\":\"USD\"}}";
+    String expectedMessage = "Unable to find the product.";
+    doThrow(new EntityNotFoundException(expectedMessage)).when(productResourceService).updateProduct(any(ProductEntity.class));
+    //then
+    this.mvc.perform(put("/products/{id}", 99).content(body).contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(status().isNotFound()).andExpect(status().reason(expectedMessage));
+  }
+
 
 
 }
